2812331c595b0d4d01ff7a9561c1b70db431accb
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-01-15 22:11:55 +0530

    conceal function.

--- main.py
+++ main.py
@@ -13,41 +13,56 @@ LSB_MASK = {
   8: (1 << 8) - 1,
 }
 
-def get_bits(text, grouping=1):
-  b = ''.join([bin(b)[2:] for b in bytes(text.encode('utf-8'))])
-  for i in range(0, len(b), nb):
-    yield b[i:i+nb]
+def get_bits(text):
+  """get_bits creates and returns the bit stream from text and returns the
+  list containing bits as string.
+  """
+  return list(''.join([bin(b)[2:].zfill(8) for b in
+              bytes(text.encode('utf-8'))]))
 
 
-def set_lsb(num, bits):
-  return num | LSB_MASK[len(bits)]
+def new_image(old_im):
+  """The function writes pixel_map at path
+  """
+  return Image.new(old_im.mode, old_im.size)
 
 
-def get_new_pixel(pixel, bits):
-  return (
-    set_lsb(pixel[0], bits),
-    set_lsb(pixel[1], bits),
-    set_lsb(pixel[2], bits)
-  )
+def conceal(in_path, out_path, text):
+  in_img, pixel_map = read_image(in_path)
+  out_img = new_image(in_img)
 
-def hide(in_path, out_path, text):
-  bits = get_bits(text)
+  for i in range(in_img.size[0]):
+    for j in range(in_img.size[1]):
+      out_img.putpixel((i, j), pixel_map[i,j])
 
-  im = Image.open(in_path)
-  print(os.path.basename(im.fp.name))
-  print(os.path.dirname(im.fp.name))
-  print(im.format, im.size, im.mode)
+  with open(out_path, 'wb') as fp:
+    out_img.save(fp)
+    out_img.close()
 
-  pixelMap = im.load()
-  img = Image.new(im.mode, im.size)
-  pixelsNew = img.load()
-  for i in range(img.size[0]):
-    for j in range(img.size[1]):
-      pixelMap[i,j] = get_new_pixel(pixelMap[i,j])
 
-  img.show()
+def read_image(path):
+  """The function read_image, reads the image from the path `path`
+  and returns the pixel map loaded in memory.
+
+  Any exceptions raised by PIL library are unhandled.
+  """
+
+  # open the image given the path
+  im = Image.open(path)
+
+  # load the pixel map.
+  pixel_map = im.load()
+
+  # log some meta information about the image
+  print('the image read from path {} has following properties'.format(path))
+  print('size: {} x {}'.format(*im.size))
+  print('format: {}'.format(im.format))
+
+  # close the file. always.
   im.close()
-  img.close()
+
+  # return the pixel map.
+  return im, pixel_map
 
 
-hide(sys.argv[1], sys.argv[2], sys.argv[3])
+conceal(sys.argv[1], sys.argv[2], "arpit")
